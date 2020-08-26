ac7750a4bb35c9b7b5c67334fcabee07e2b01012
Jordan Belfort <felix314159@users.noreply.github.com>
2020-08-26 17:39:10 +0200

    Add files via upload

---
+++ rename_files.py
@@ -0,0 +1,15 @@
+import os
+
+def remove_first_characters(a):
+    a = abs(a)
+    for i in sorted(os.listdir(os.getcwd())):
+        name = os.path.splitext(i)[0]
+        # filetype 0 means that the file has no type, should be a folder in most cases
+        filetype = os.path.splitext(i)[1] if len(os.path.splitext(i)[1]) > 0 else "0"
+        
+        if filetype != "0" and filetype != ".py" and len(name) - a > 0:
+            os.rename(i, i[a::])
+
+
+# remove the first 3 letters of every file in current directory that isnt a folder or a python script
+remove_first_characters(1)
